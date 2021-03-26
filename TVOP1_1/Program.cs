07fca45383b26a3914a961e67d51cb495f753ffb
olelit <litasov00@gmail.com>
2021-03-26 23:45:09 +0300

    Добавьте файлы проекта.

---
+++ TVOP1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TVOP1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var helloWorld = "Hello World!";
+            var andHiaGain = "Andhiagain!";
+            var vosklSnak = new String('!', rand.Next(5, 50));
+            Console.WriteLine(string.Format("{0}\n{1}\n{2}", helloWorld, andHiaGain, vosklSnak));
+        }
+    }
+}
